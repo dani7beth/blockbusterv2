cd47d0476d09621767126d3f384f41d3afd86c71
dani7beth <danielledona28@gmail.com>
2021-09-24 19:31:33 -0600

    added frontend

---
+++ client/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Taco from "./taco";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <Taco />
+    </>
+  );
+};
+export default Home;
---
+++ client/src/Pages/taco.js
@@ -0,0 +1,5 @@
+import React from "react";
+const Taco = () => {
+  return <h1>TACOS!</h1>;
+};
+export default Taco;
